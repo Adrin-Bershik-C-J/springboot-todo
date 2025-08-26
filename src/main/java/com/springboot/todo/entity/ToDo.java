b967b681e2144dd22d7d027f4e135ee5123bbf0b
Adrin-Bershik-C-J <cjabershik@gmail.com>
2025-08-26 21:58:31 +0530

    feat:added user entity and repository

--- src/main/java/com/springboot/todo/entity/ToDo.java
+++ src/main/java/com/springboot/todo/entity/ToDo.java
@@ -11,6 +11,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -36,4 +38,8 @@ public class ToDo {
 
     @Enumerated(EnumType.STRING)
     private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
